c0e22547a3006482cc4bd0880c218726ab41eb90
Phyroks <52051341+Phyroks@users.noreply.github.com>
2021-02-22 01:44:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts
 rm -rf main.css
-echo "@import url('https://raw.githubusercontent.com/Manager-IT-eu/theme/main/user.css')" >> /var/www/pterodactyl/resources/scripts/main.css
+echo "@import url('https://hosting-network.eu/user.css')" >> /var/www/pterodactyl/resources/scripts/main.css
 rm -rf index.tsx
 wget https://raw.githubusercontent.com/DeveloperNeon/ptero-1.0-theme-install/main/resources/index.tsx
 cd ../..
@@ -9,11 +9,13 @@ php artisan view:clear
 php artisan cache:clear
 clear
 echo "User panel theme has been added."
+echo "Installing admin theme."
+echo "Installing admin theme.."
 echo "Installing admin theme..."
+echo "Installing admin theme...."
 cd /var/www/pterodactyl/resources/views/layouts/
 rm -rf admin.blade.php
 wget https://raw.githubusercontent.com/Manager-IT-eu/theme/main/admin.blade.php
-npm install yarn -g
 yarn install
 yarn add @emotion/react
 rm -rf /var/www/pterodactyl/resources/scripts/hoc/requireServerPermission.tsx
